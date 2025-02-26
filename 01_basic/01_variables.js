9ef01efabed3c906e2395fa740edf9976f64f1a9
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-02-26 18:31:30 +0000

    const and console.table

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId= 144553
+let accountEmailId = "hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId = 2   //Not allowed
+
+accountEmailId ="hello@gamil.com"
+console.log(accountId);
+
+console.table([accountId, accountEmailId, accountPassword])
