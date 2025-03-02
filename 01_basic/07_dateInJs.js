5d6e943f3d1e80270da93f95ee9b067a6153dc5a
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-02 17:49:45 +0000

    Date format

--- 01_basic/07_dateInJs.js
+++ 01_basic/07_dateInJs.js
@@ -2,6 +2,18 @@
 
 
 let myDate  = new Date()
-console.log(myDate); // 
-console.log(myDate.toString());
-console.log(myDate.toLocaleDateString);
+// console.log(myDate); // 
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2025, 0, 23);
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDate1 = new Date(2025, 0, 23,5,3);
+console.log(myCreatedDate1.toDateString());
+
+let myCreatedDate2 = new Date("2025-03-02");
+console.log(myCreatedDate2.toLocaleString());
