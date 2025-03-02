6c5067c4bde6d0b677861ed260e9c4bff7b5ce9e
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-02 17:59:38 +0000

    Date format

--- 01_basic/07_dateInJs.js
+++ 01_basic/07_dateInJs.js
@@ -21,3 +21,19 @@ console.log(myCreatedDate2.toLocaleString());
 
 let myCreatedDate3 = new Date("03-02-2025");
 console.log(myCreatedDate3.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate3.getTime());
+console.log(Math.floor(Date.now()/1000));
+console.log(".....................................................");
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday : "long",
+    
+})
