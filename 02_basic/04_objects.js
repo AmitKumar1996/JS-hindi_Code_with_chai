273c758e82eb142a0f19b8e817af9c4e3f1ce5c3
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-03 19:51:22 +0000

    object part -2

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,13 @@
+// What is differance b/w Singelton  Non Singelton Objects write one use case 
+const tinderUser = new Object() // Singelton Objects
+
+const tinderUser1 = {} // Non Singelton Objects
+
+console.log(tinderUser);
+console.log(tinderUser1);
+
+tinderUser.id = "123abc"
+tinderUser.name="Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser)
