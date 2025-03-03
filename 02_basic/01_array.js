af31ae55d67f68a1935e890117412694b5900ee9
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-03 05:03:41 +0000

    JS Important Topics

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -1,3 +1,24 @@
+/** JS Important Topic
+ * var, Let, Const
+ * Hosting
+ * Debouncing
+ * Prototype Inheritence
+ * Closures
+ * Event Loop
+ */
+
+/** React Important Topics 
+ * Hooks
+ * Performance Optimization
+ * Vertual DOM
+ * React Suspense
+ * State Management
+ */
+
+
+
+
+
 // Array
 
 const myArr = [0, 1,2,3,4,5]
