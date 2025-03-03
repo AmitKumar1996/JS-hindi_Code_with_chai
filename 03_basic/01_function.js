2f6f806b54513dc08a6149c2982f606405fd0759
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-03 22:15:27 +0000

    function part -1

---
+++ 03_basic/01_function.js
@@ -0,0 +1,17 @@
+// what is function resrance defnition execute?
+
+function MyFunction(){
+    console.log("A");
+    console.log("M");
+    console.log("I");
+    console.log("T");
+}
+MyFunction() 
+MyFunction
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(3,"4")
+addTwoNumbers(3,4)
+addTwoNumbers(3, null)
