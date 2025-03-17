f45a798808ddb7df4a7bfad49e8b0d303b09f649
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-17 23:56:26 +0000

    Digital clock

---
+++ 07_project/DigitalClock/digitalClock.js
@@ -0,0 +1,16 @@
+const clock = document.getElementById('clock');
+
+setInterval(function () {
+  // Create a new Date object
+  let date = new Date();
+
+  // Get the date and time as strings
+  const currentDate = date.toLocaleDateString();
+  const currentTime = date.toLocaleTimeString();
+
+  // Combine the date and time into a single string
+  const dateTimeString = `${currentDate} ${currentTime}`;
+
+  // Update the innerHTML of the clock element
+  clock.innerHTML = dateTimeString;
+}, 1000);
