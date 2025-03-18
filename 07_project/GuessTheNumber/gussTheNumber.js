bf91483e93eab655ece258d5b70a1b4e161695b5
Amit Kumar <32728053+AmitKumar1996@users.noreply.github.com>
2025-03-18 01:08:36 +0000

    guss the number

--- 07_project/GuessTheNumber/gussTheNumber.js
+++ 07_project/GuessTheNumber/gussTheNumber.js
@@ -1,2 +1,17 @@
 const random = parseInt( Math.random() * 1000 +1 )
-console.log(random);
+
+const submit = document.querySelector( '#subt' )
+
+const userInput = document.querySelector( '#guessField' )
+
+const gussSlot = document.querySelector( '.guesses' )
+
+
+const remaning = document.querySelector( '.lastResult' )
+
+
+
+
+
+
+
