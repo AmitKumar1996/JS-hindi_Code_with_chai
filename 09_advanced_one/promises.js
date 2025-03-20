846fb933e4837c0db1db4e4681779cfdefde42aa
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-03-21 05:04:10 +0530

    promises

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -29,3 +29,42 @@ new Promise( function ( resolve, reject )
     console.log( "Async 2 resolve" );
 
 } )
+
+const PromiseThree = new Promise( function ( resolve, reject )
+{
+    setTimeout( function ()
+    {
+        resolve( { userNmae: "chai", email: "amit@123.com" } )
+
+    }, 1000 )
+} )
+
+PromiseThree.then( function ( userData )
+{
+    console.log( userData );
+
+} )
+
+const PromiseFour = new Promise( function ( resolve, reject )
+{
+    setTimeout( function ()
+    {
+        let error = false
+        if ( error )
+        {
+            resolve( { userNmae: "AmitJi", password: "123" } )
+        } else
+        {
+            reject( 'Some thing want wroing' )
+        }
+    }, 2000 )
+
+} )
+
+const usernmae = PromiseFour.then( ( user ) =>
+{
+    console.log( user );
+    return user.usernmae
+
+} )
+console.log( usernmae );
