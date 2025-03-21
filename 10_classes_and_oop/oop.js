d2c1d70571325284a58b9846195351a465fd9b0e
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-03-22 02:47:31 +0530

    OOPs

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -15,4 +15,29 @@ const user = {
 }
 console.log( user.username );
 console.log( user.getUserDetails() );
+console.log( this );
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+// constructer
+function User( username, loginCount, isLoggedIn )
+{
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+    return this;
+}
+
+const userOne = new User( "Amit", 12, true )
+const userTwo = new User( "AmitTheCoder", 11, false )
+console.log( userOne );
+console.log( userTwo );
+console.log( userOne.constructor ); // what is this ?
+
+
+// what is new Key word
+// what is constructer function and why this used ?
+
+
 
